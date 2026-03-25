135acc3b5ce1ba4a4e9969e27c801627e090b34c
Ksahitha <sxk59170@ucmo.edu>
2026-03-24 19:57:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import gradio as gr
+from TTS.api import TTS
+
+tts=TTS("tts_models/multilingual/multi-dataset/your_tts")
+
+def clone(audio,text):
+
+    output="output.wav"
+
+    tts.tts_to_file(
+
+        text=text,
+        speaker_wav=audio,
+        language="en",
+        file_path=output
+
+    )
+
+    return output
+
+
+interface=gr.Interface(
+
+    fn=clone,
+
+    inputs=[
+
+        gr.Audio(type="filepath"),
+        gr.Textbox(label="Enter text")
+
+    ],
+
+    outputs=gr.Audio()
+
+)
+
+interface.launch()
